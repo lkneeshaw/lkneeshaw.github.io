80be51e989e45d4dfb4718e08461f282745d5742
lkneeshaw <lrkneeshaw@yahoo.com>
2018-04-20 12:58:27 -0700

    Added content
    - Hero background image
    - text content
    - links to prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
-// @codekit-prepend "jquery.js";
-// @codekit-prepend "semantic.js";
-
-console.log('Hello, World!');
+//$(window).scroll(function() {
+//  var scrollTop = $(this).scrollTop();
+//
+//    $('nav').css({
+//      opacity: function() {
+//        var elementHeight = $(window).height(),
+//            opacity = ((1 - (elementHeight - scrollTop) / elementHeight));
+//
+//        return opacity;
+//      }
+//    });
+//});
